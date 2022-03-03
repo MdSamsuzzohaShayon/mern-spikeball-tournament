61b2b1953c3b58240ec407b6838473d968ed87d2
Md Samsuzzoha <mdshayon0@gmail.com>
2022-03-03 14:45:46 +0600

    delete backup

--- client/src.backup/components/elements/Loader.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-
-const Loader = () => (<div className="text-center spinner-parent">
-    <div className="spinner-border text-danger spinner-child" role="status">
-    </div>
-</div>)
-
-export default Loader;
