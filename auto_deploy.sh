3cb23d0f2f83880c4ae67bacbaf7772bd7372e9e
Md Shayon <mdshayon0@gmail.com>
2025-02-16 17:08:50 +0600

    Load balancing properly using apache

--- auto_deploy.sh
+++ auto_deploy.sh
@@ -1,31 +1,54 @@
 #!/bin/bash
 
-# Deploy Backend
-echo "Deploy Node js Server"
-pm2 stop BackendAPI
-pm2 delete BackendAPI
-pm2 flush
-pm2 save
 
-rm -rf mern-spikeball-tournament
+# Update 
+sudo apt update -y 
+sudo apt upgrade -y
+
+# Delete previous server
+echo "Delete all previous server"
+pm2 stop all
+pm2 delete all
+pm2 flush
+rm -rf /home/shayon/youthspikersleague.com
+sudo rm -rf  /var/www/youthspikersleague.com
+cd 
+rm -rf youthspikersleague.com
+ls 
+ls -la 
+ls -la /var/www/
+echo "Deleted all previous files for the website"
+
+# Clone and setup repository
 git clone git@github.com:MdSamsuzzohaShayon/mern-spikeball-tournament.git
-cd /home/alex/mern-spikeball-tournament
-cd server
+mv mern-spikeball-tournament youthspikersleague.com
+
+# Setup backend
+cd /home/shayon/youthspikersleague.com/server
 npm install
+cat config/.env.example
 nano config/.env
 pm2 start pm2.ecosystem.json
-pm2 save --force
-pm2 startup
-sudo env PATH=$PATH:/usr/bin /usr/lib/node_modules/pm2/bin/pm2 startup systemd -u alex --hp /home/alex
+pm2 save
+sudo env PATH=$PATH:/usr/bin /usr/lib/node_modules/pm2/bin/pm2 startup systemd -u shayon --hp /home/shayon
+pm2 show Scramble_BackendAPI_9001
+pm2 show Scramble_BackendAPI_9002
+pm2 show Scramble_BackendAPI_9003
+pm2 show Scramble_BackendAPI_9004
 
-# Deploy frontend
-echo "Deploy React Client"
-cd /home/alex/mern-spikeball-tournament/client
+# Setup frontend
+cd /home/shayon/youthspikersleague.com/client
+# /var/www/youthspikersleague.com/html
+# cat /etc/nginx/sites-available/youthspikersleague.com
 npm install
 nano src/utils/global.ts
 npm run build
-ls
-sudo rm -rf /var/www/youthspikersleague.com
-sudo cp -R dist /var/www/youthspikersleague.com
-sudo cp /home/alex/mern-spikeball-tournament/apache/var/www/youthspikersleague.com/.htaccess /var/www/youthspikersleague.com/.htaccess
-ls -la /var/www/youthspikersleague.com
+sudo mkdir /var/www/youthspikersleague.com
+sudo cp -r dist/* /var/www/youthspikersleague.com
+rm -rf node_modules
+
+# Test
+curl --location 'https://youthspikersleague.com/api/home'
+curl --location 'https://youthspikersleague.com/api/home'
+curl --location 'https://youthspikersleague.com/api/home'
+curl --location 'https://youthspikersleague.com/api/home'
--- server/routes/index.js
+++ server/routes/index.js
@@ -4,7 +4,7 @@ const router = express.Router();
 
 
 router.get('/home', (req, res, next) => {
-    res.status(200).json({ "Msg": "success" });
+    res.status(200).json({ "Msg": `Testing server on port: ${process.env.PORT}` });
 });
 
 
